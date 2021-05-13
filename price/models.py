9a6a55847a32f6362373546fd4a5e37a7af6d80a
lowity <seya.fusion@gmail.com>
2021-05-13 22:09:20 +0200

    V 1.7

--- price/models.py
+++ price/models.py
@@ -2,8 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class Time(models.Model):
-    time = models.TimeField("max_length")
-
-class Price(models.Model):
-    price = models.CharField(max_length=200)
